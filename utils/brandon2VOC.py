8ecf890665de8668c9591a6ba81abdacb47eb665
Liam Berti <ltb16@pitt.edu>
2018-06-09 19:24:33 -0400

    added converter for two formats

--- utils/brandon2VOC.py
+++ utils/brandon2VOC.py
@@ -5,36 +5,32 @@ import pascal_voc_writer as pvw
 import os
 import sys
 """
-To use run: python YOLO2VOC.py yoloV2annotation.txt
+To use run: python brandon2VOC.py yoloV2annotation.txt
 Requires annotations and images to be in the same directory with the same names
 before the extenstion
 """
 
-#create YOLO label
-class YoloLabel():
-    def __init__(self,cn,x1,y1,w,h):
+#create brandon label
+class BrandonLabel():
+    def __init__(self,x1,y1,x2,y2,cn):
         #class_number x1_ratio y1_ratio width_ratio height_ratio
         self.class_number = cn
-        self.x1_ratio = float(x1)
-        self.y1_ratio = float(y1)
-        self.width_ratio = float(w)
-        self.height_ratio = float(h)
+        self.x1 = float(x1)
+        self.y1 = float(y1)
+        self.x2 = float(x2)
+        self.y2 = float(y2)
 
-    def yolo2voc(self,imWidth,imHeight):
-        centerX = self.x1_ratio * imWidth
-        centerY = self.y1_ratio* imHeight
-        bboxWidth = self.width_ratio * imWidth
-        bboxHeight = self.height_ratio * imHeight
+    def bran2voc(self):
         #VOC
         #<class> <bboxXMin> <bboxYMin> <bboxXMax> <bboxYMax>
         voc = [0,0,0,0]
-        voc[0] = centerX - (bboxWidth/2)
-        voc[1] = centerY - (bboxHeight/2)
-        voc[2] = centerX + (bboxWidth/2)
-        voc[3] = centerY + (bboxHeight/2)
+        voc[0] = self.x1
+        voc[1] = self.y1
+        voc[2] = self.x2
+        voc[3] = self.y2
         return voc
 
-class YoloFile():
+class BrandonFile():
     def __init__(self,fname):
         self.fname  = fname  + ".txt"
         self.imname = fname + ".jpg"
@@ -51,7 +47,7 @@ class YoloFile():
             #read line
             yolo_params = line.split()
             #append line to queue
-            self.yl_list.append(YoloLabel(yolo_params[0],yolo_params[1],\
+            self.yl_list.append(BrandonLabel(yolo_params[0],yolo_params[1],\
                     yolo_params[2],yolo_params[3],yolo_params[4]))
         f.close()
 
@@ -59,14 +55,14 @@ class YoloFile():
         writer = pvw.Writer(self.imname,self.width,self.height)
         while(self.yl_list):
             obj_label = self.yl_list.pop()
-            voc = obj_label.yolo2voc(self.width,self.height)
+            voc = obj_label.bran2voc()
             writer.addObject(obj_label.class_number,voc[0],voc[1],voc[2],voc[3])
 
         writer.save(fname + ".xml")
 
 fname = sys.argv[1]
 fname,ext=os.path.splitext(fname)
-yFile = YoloFile(fname)
+yFile = BrandonFile(fname)
 yFile.readr()
 yFile.writer()
 
